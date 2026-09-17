736bc68742c1638d4652cea3e9c20f3fa46c58cb
Roman Lemeshko <107170410+zxcresp@users.noreply.github.com>
2026-09-17 21:15:29 +0500

    Implement password generation features

--- Password Generator/password_generator.py
+++ Password Generator/password_generator.py
@@ -1,6 +1,9 @@
 import secrets
 import string
 #github: https://github.com/zxcresp
+
+#y = True
+#n = False
 #============ASK USER FOR ALL PASSWORD INFORMATION============
 
 def get_length():
@@ -16,18 +19,65 @@ def get_digits():
     while True:
         digits_yn = input('Do you want the digits? (Y/N): ')
 
-        if digits_yn.lower() == 'y' or digits_yn.lower() == 'n':
-            break
+        if digits_yn.lower() == 'y':
+            return True
+        if digits_yn.lower() == 'n':
+            return False
 
         print('Enter only "y" or "n"')
 
 def get_specials():
     while True:
-        pass
+        specials_yn = input('Do you want the special characters? (Y/N): ')
+
+        if specials_yn.lower() == 'y':
+            return True
+        if specials_yn.lower() == 'n':
+            return False
+
+        print('Enter only "y" or "n"')
 
 def get_letters():
     while True:
-        pass
+        letters_yn = input('Do you want the letters? (Y/N): ')
 
-lenght = get_length()
-digits = get_digits()
+        if letters_yn.lower() == 'y':
+            return True
+        if letters_yn.lower() == 'n':
+            return False
+
+        print('Enter only "y" or "n"')
+
+def create_chars(letters, digits, specials):
+    chars = ""
+    if letters:
+        chars += string.ascii_letters
+    if digits:
+        chars += string.digits
+    if specials:
+        chars += string.punctuation
+
+    return chars
+
+def generate_password(length, chars):
+    password = ""
+
+    for _ in range(length):
+        password += secrets.choice(chars)
+
+    return password
+
+#============FUNCTION CALLS============
+while True:
+    length = get_length()
+    digits = get_digits()
+    specials = get_specials()
+    letters = get_letters()
+    chars = create_chars(letters, digits, specials)
+    if not chars:
+        print("At least one character type must be selected")
+        continue
+    break
+
+password = generate_password(length, chars)
+print(password)
